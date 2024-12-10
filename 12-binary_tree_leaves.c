6c8414408c2ae99871943dd61eccb0157ef3fdc4
Noam72T <9823@holbertonstudents.com>
2024-12-10 15:07:10 +0100

    C - Binary Trees Exercice 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,18 +3,16 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_leaves - Function tree size
+ * binary_tree_leaves - Function tree leaves
  * @tree: Pointer to the root node
  */
 
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-    if (!tree)
-        return 0;
+size_t binary_tree_leaves(const binary_tree_t *tree) {
+  if (!tree)
+    return 0;
 
-    if (!tree->left && !tree->right)
-        return 1;
+  if (!tree->left && !tree->right)
+    return 1;
 
-   
-    return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+  return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 }
